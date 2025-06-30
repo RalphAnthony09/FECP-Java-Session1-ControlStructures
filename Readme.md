81a3d3d404f9386866c40c8f8c6c89bf4f555e6a
Ralph Anthony  Rafael <ralphanthony.rafael@LAPTOP-MYNT607.mynt.asticom.com.ph>
2025-06-30 16:22:36 +0800

    Final Submission, Lab 2: Membership Calculator

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Lab 1: Celsius to Fahrenheit Converter
+
+**Name:** Ralph Anthony R. Rafael </br>
+**Project Title:** Lab 2: Membership Calculator </br>
+**Screenshot of output:** </br>
+
+![Lab2 Diagram](Lab2.png)
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,50 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.printf("Enter membership status (Regular, VIP, Premium): ");
+        String status = scanner.nextLine().trim();
+
+        System.out.printf("Enter age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        double price = 0.00;
+
+        if (status.equalsIgnoreCase("Regular")) {
+            if (age < 18) {
+                price = 50.00;
+            } else if (age <= 64) {
+                price = 100.00;
+            } else {
+                price = 75.00;
+            }
+        } else if (status.equalsIgnoreCase("VIP")) {
+            if (age < 18) {
+                price = 75.00;
+            } else if (age <= 64) {
+                price = 150.00;
+            } else {
+                price = 112.50;
+            }
+        } else if (status.equalsIgnoreCase("Premium")) {
+            if (age < 18) {
+                price = 100.00;
+            } else if (age <= 64) {
+                price = 200.00;
+            } else {
+                price = 150.00;
+            }
+        } else {
+            System.out.println("Invalid membership status entered.");
+            scanner.close();
+            return;
+        }
+
+
+        System.out.printf("Price: $%.2f%n", price);
+
+        scanner.close();
     }
 }
